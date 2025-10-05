9ae77a6b4ec7781bfbf34f58c81254e39285d206
azlankh <azlan.k.harris@gmail.com>
2025-10-05 19:51:00 +0530

    Add files via upload

---
+++ power_calculator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Power Calculator!")
+num1 = int(input("Enter a Number: "))
+num2 = int(input("Enter The Power You Want To Raise The Number To: "))
+sum = num1 ** num2
+print("The Result Is: ",sum)
